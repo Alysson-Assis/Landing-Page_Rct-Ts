677265d6ce0f690e605e0521f1482b4beb9ebd51
Alysson Santos <alyssonassissantos@gmail.com>
2023-12-05 20:24:46 -0300

    feat ( Criação Componentes - 20:22 ): Criado os componentes TopBar, Home, Objetio, Projetos, Membros e Contatos

---
+++ src/componentes/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Objetivo</li>
+                <li>Projetos</li>
+                <li>Membros</li>
+                <li>Contatos</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/componentes/Objetivo/Objetivo.tsx
@@ -0,0 +1,9 @@
+const Objetivo = () => {
+    return (
+        <div>
+            <h1>Objetivo</h1>
+        </div>
+    );
+}
+
+export default Objetivo;
