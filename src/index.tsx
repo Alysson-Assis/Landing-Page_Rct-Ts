bb53feca5997b0149f0348cd1d27b2140fd97fc1
Alysson Santos <alyssonassissantos@gmail.com>
2023-12-05 22:13:58 -0300

    fix ( Decorete module - 21:20 ): Usei o decorete module para ativar o formato de arquivo ( png, jpg )

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+declare module "*.jpg"
+declare module "*.png"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
