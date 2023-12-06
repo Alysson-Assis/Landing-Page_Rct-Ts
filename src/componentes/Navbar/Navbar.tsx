344e6b24c6ed9af3ac3a8ae6f09037330c8b1635
Alysson Santos <alyssonassissantos@gmail.com>
2023-12-05 21:36:26 -0300

    feat ( Navbar componentes; Logo app - 21:35 ): Coloquei o logo do site; Criei o nome da pagina; Passei os componentes do site em forma de lista

--- src/componentes/Navbar/Navbar.tsx
+++ src/componentes/Navbar/Navbar.tsx
@@ -1,14 +1,19 @@
-
+import logo from "../../images/Octopus.jpg"
+import styles from "./Navbar.module.css"
 
 const Navbar = () => {
+
+
     return (
         <div>
+            <img src={logo} className={styles.logoPage} alt="HydraOctopus_logo" />
+            <h1>HydraOctopus</h1>
             <ul>
-                <li>Home</li>
-                <li>Objetivo</li>
-                <li>Projetos</li>
-                <li>Membros</li>
-                <li>Contatos</li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Objetivo</a></li>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Membros</a></li>
+                <li><a href="">Contatos</a></li>
             </ul>
         </div>
     );
